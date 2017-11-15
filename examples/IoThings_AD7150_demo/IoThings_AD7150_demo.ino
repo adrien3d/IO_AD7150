42f8a8755d70207d021a6d31ca7f0ff7f73933ca
Adrien Chapelet <adrien3d@gmail.com>
2017-11-15 22:50:39 +0100

    Rerebranding

---
+++ examples/IoThings_AD7150_demo/IoThings_AD7150_demo.ino
@@ -0,0 +1,33 @@
+#include "IO_AD7150.h"
+
+IO_AD7150 ad7150;
+AD7150_Values result;
+
+void setup()
+{
+  ad7150.begin();
+  ad7150.setOffset(AD7150_OFFSET_DAC_EN_MANU_5);
+  ad7150.setRange(AD7150_CAPACTIVE_INPUT_RANGE_2);
+  ad7150.setup();
+
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  ad7150.configure(AD7150_MODE_SING_CONV);
+  delay(10);
+
+  ad7150.configure(AD7150_MODE_POWER_DOWN); 
+
+  result = ad7150.getValue();
+
+  /*Serial.print("Status :");
+  Serial.print(result.status, BIN);
+  Serial.print("\t");*/
+  Serial.print("Value :");
+  Serial.print(result.value, DEC);
+  
+  delay(20);
+  Serial.println();
+}
